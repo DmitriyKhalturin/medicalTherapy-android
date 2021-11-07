91d38a11b22ccc2d1e4e79ca9e07cf663b374feb
Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
2021-11-08 01:10:29 +0300

    Update colors, remove styles. Update compose themes.

--- app/src/main/kotlin/org/medicine/ui/theme/Shape.kt
+++ app/src/main/kotlin/org/medicine/ui/theme/Shape.kt
@@ -1,11 +1,69 @@
 package org.medicine.ui.theme
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Shapes
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
+  small = RoundedCornerShape(4.dp),
+  medium = RoundedCornerShape(4.dp),
+  large = RoundedCornerShape(0.dp)
 )
+
+@Preview(showBackground = true)
+@Composable
+fun SmallShapePreview() {
+  MedicineTheme {
+    MaterialTheme.colors.apply {
+      Sample(primary, onPrimary, Shapes.small)
+    }
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MediumShapePreview() {
+  MedicineTheme {
+    MaterialTheme.colors.apply {
+      Sample(secondary, onSecondary, Shapes.medium)
+    }
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LargeShapePreview() {
+  MedicineTheme {
+    MaterialTheme.colors.apply {
+      Sample(surface, onSurface, Shapes.large)
+    }
+  }
+}
+
+@Composable
+fun Sample(surfaceColor: Color, contentColor: Color, shape: Shape) {
+  Surface(
+    color = MaterialTheme.colors.surface,
+    modifier = Modifier.padding(16.dp),
+  ) {
+    Surface(
+      color = surfaceColor,
+      shape = shape,
+      contentColor = contentColor,
+    ) {
+      Text(
+        text = "Sample",
+        modifier = Modifier.padding(8.dp),
+      )
+    }
+  }
+}
--- app/src/main/kotlin/org/medicine/ui/theme/Theme.kt
+++ app/src/main/kotlin/org/medicine/ui/theme/Theme.kt
@@ -2,43 +2,36 @@ package org.medicine.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
+import org.medicine.R
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
+private val LightColorPalette
+  @Composable get() = lightColors(
+    primary = colorResource(id = R.color.mediumRose),
+    primaryVariant = colorResource(id = R.color.rose),
+    onPrimary = colorResource(id = R.color.haleNavy),
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = colorResource(id = R.color.greenishBlue),
+    secondaryVariant = colorResource(id = R.color.grayTurquoise),
+    onSecondary = colorResource(id = R.color.seashell),
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
+    background = colorResource(id = R.color.background),
+    onBackground = colorResource(id = R.color.haleNavy),
+
+    surface = colorResource(id = R.color.lightGray),
+    onSurface = colorResource(id = R.color.darkGray),
+  )
 
 @Composable
-fun MedicineTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun MedicineTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+  val colors = LightColorPalette
 
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+  MaterialTheme(
+    colors = colors,
+    typography = Typography,
+    shapes = Shapes,
+    content = content
+  )
 }
