31ea9e442db8f3f7341b09573cfa0673380de6ef
Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
2021-11-11 00:53:17 +0300

    Create screens skels.

---
+++ app/src/main/kotlin/org/medicine/ui/screen/AboutScreen.kt
@@ -0,0 +1,12 @@
+package org.medicine.ui.screen
+
+import androidx.compose.runtime.Composable
+
+/**
+ * Created by Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
+ * for Medicine on 11.11.2021 0:01.
+ */
+
+@Composable
+fun AboutScreen() {
+}
---
+++ app/src/main/kotlin/org/medicine/ui/screen/EventScreen.kt
@@ -0,0 +1,12 @@
+package org.medicine.ui.screen
+
+import androidx.compose.runtime.Composable
+
+/**
+ * Created by Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
+ * for Medicine on 10.11.2021 23:57.
+ */
+
+@Composable
+fun EventScreen() {
+}
