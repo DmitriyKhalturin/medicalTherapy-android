dcca5abb401dbc99ee39129229345a4bafae0373
Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
2021-11-11 00:53:33 +0300

    Create viewModels skels.

---
+++ app/src/main/kotlin/org/medicine/viewmodel/DayScheduleViewModel.kt
@@ -0,0 +1,13 @@
+package org.medicine.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
+ * for Medicine on 11.11.2021 0:40.
+ */
+class DayScheduleViewModel: ViewModel() {
+
+  companion object {
+  }
+}
---
+++ app/src/main/kotlin/org/medicine/viewmodel/ScheduleViewModel.kt
@@ -0,0 +1,10 @@
+package org.medicine.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
+ * for Medicine on 11.11.2021 0:24.
+ */
+class ScheduleViewModel: ViewModel() {
+}
