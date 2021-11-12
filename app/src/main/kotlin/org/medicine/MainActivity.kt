046bbedc2ace7492e7de5a9403fdaef80a3a277c
Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
2021-11-12 20:10:34 +0300

    Add scaffold state remember.

--- app/src/main/kotlin/org/medicine/MainActivity.kt
+++ app/src/main/kotlin/org/medicine/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
@@ -18,10 +19,13 @@ class MainActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
 
     setContent {
+      val scaffoldState = rememberScaffoldState()
       val navController = rememberNavController()
 
       MedicineTheme {
-        Scaffold { innerPadding ->
+        Scaffold(
+          scaffoldState = scaffoldState,
+        ) { innerPadding ->
           NavHost(
             navController = navController,
             graph = buildNavGraph(navController),
