356bfdc234367aadf71f9552e2c80b81bfbbca6d
Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
2021-11-13 09:09:37 +0300

    Update overview screen.

--- app/src/main/kotlin/org/medicine/ui/screen/overview/OverviewScreen.kt
+++ app/src/main/kotlin/org/medicine/ui/screen/overview/OverviewScreen.kt
@@ -1,9 +1,22 @@
 package org.medicine.ui.screen.overview
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import org.medicine.navigation.Route
+import org.medicine.tools.EMPTY_STRING
+import org.medicine.ui.screen.overview.composable.FetchingTherapies
+import org.medicine.ui.screen.overview.composable.NoOneTherapies
+import org.medicine.ui.screen.overview.composable.Therapies
 import org.medicine.ui.screen.overview.model.OverviewViewState
+import org.medicine.ui.theme.MedicineTheme
 
 /**
  * Created by Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
@@ -12,23 +25,65 @@ import org.medicine.ui.screen.overview.model.OverviewViewState
 
 @Composable
 fun OverviewScreen(
-  modifier: Modifier = Modifier,
   navController: NavController,
   viewModel: OverviewViewModel,
 ) {
   Overview(
+    navController,
     viewModel.uiState,
   )
 }
 
 @Composable
-private fun Overview(
+fun Overview(
+  navController: NavController,
   uiState: OverviewViewState,
 ) {
-  when (uiState) {
-    is OverviewViewState.Initial -> Unit
-    is OverviewViewState.FetchingTherapies -> Unit
-    is OverviewViewState.NoOneTherapies -> Unit
-    is OverviewViewState.Therapies -> Unit
+  val scaffoldState = rememberScaffoldState()
+
+  Scaffold(
+    scaffoldState = scaffoldState,
+    topBar = {
+      OverviewTopBar(
+        applicationOnClick = {
+          navController.navigate(Route.ApplicationInfo.name)
+        }
+      )
+    }
+  ) { innerPadding ->
+    Box(modifier = Modifier.padding(innerPadding)) {
+      when (uiState) {
+        is OverviewViewState.Initial -> Unit
+        is OverviewViewState.FetchingTherapies -> FetchingTherapies()
+        is OverviewViewState.NoOneTherapies -> NoOneTherapies()
+        is OverviewViewState.Therapies -> Therapies()
+      }
+    }
+  }
+}
+
+@Composable
+fun OverviewTopBar(
+  applicationOnClick: () -> Unit,
+) {
+  TopAppBar(
+    title = {},
+    actions = {
+      IconButton(onClick = { applicationOnClick() }) {
+        Icon(Icons.Outlined.Info, EMPTY_STRING)
+      }
+    },
+  )
+}
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun OverviewPreview() {
+  MedicineTheme {
+    Overview(
+      rememberNavController(),
+      OverviewViewState.FetchingTherapies,
+    )
   }
 }
---
+++ app/src/main/kotlin/org/medicine/ui/screen/overview/composable/NoOneTherapies.kt
@@ -0,0 +1,12 @@
+package org.medicine.ui.screen.overview.composable
+
+import androidx.compose.runtime.Composable
+
+/**
+ * Created by Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
+ * for Medicine on 13.11.2021 8:30.
+ */
+
+@Composable
+fun NoOneTherapies() {
+}
