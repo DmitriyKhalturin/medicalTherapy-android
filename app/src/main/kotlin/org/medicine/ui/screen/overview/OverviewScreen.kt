e231dd34861c6754a80fd9dd9267da49e39a3d7e
Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
2021-11-25 13:16:47 +0300

    Clear code.

--- app/src/main/kotlin/org/medicine/ui/screen/overview/OverviewScreen.kt
+++ app/src/main/kotlin/org/medicine/ui/screen/overview/OverviewScreen.kt
@@ -10,14 +10,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import net.cubix.datepicker.ReelDatePicker
-import net.cubix.datepicker.theme.ReelDatePickerColors
-import net.cubix.datepicker.theme.ReelDatePickerTheme
 import org.medicine.navigation.Destination
 import org.medicine.navigation.navigate
 import org.medicine.tools.EMPTY_STRING
@@ -26,7 +21,6 @@ import org.medicine.ui.screen.overview.composable.Therapies
 import org.medicine.ui.screen.overview.model.OverviewIntent
 import org.medicine.ui.screen.overview.model.OverviewViewState
 import org.medicine.ui.theme.MedicalTherapyTheme
-import java.time.LocalDate
 
 /**
  * Created by Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
@@ -74,7 +68,7 @@ fun Overview(
       contentAlignment = Alignment.Center
     ) {
       when (uiState) {
-        is OverviewViewState.Initial -> TestReelDatePicker()
+        is OverviewViewState.Initial -> Unit
         is OverviewViewState.NoOneTherapies ->
           NoOneTherapies(
             createTherapyOnClick = {
@@ -94,33 +88,6 @@ fun Overview(
           )
       }
     }
-  }
-}
-
-@Composable
-fun TestReelDatePicker() {
-  val date = LocalDate.of(2021, 11, 27)
-  val startDate = date.minusDays(2)
-  val endDate = date.plusDays(5)
-
-  ReelDatePickerTheme(
-    colors = ReelDatePickerColors(
-      widgetColor = Color(0xFFE2E1E1),
-      itemDefaultColor = Color(0xFF3C3C3D),
-      itemSelectedColor = Color(0xFF232936),
-    )
-  ) {
-    ReelDatePicker(
-      datePickerTitle = "Date:",
-      numberPickerTitle = "Day",
-      modifier = Modifier.padding(16.dp),
-      startDate = startDate,
-      endDate = endDate,
-      selectedDate = date,
-      dateOnChange = {
-        //
-      }
-    )
   }
 }
 
