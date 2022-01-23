e5abd703e1cb35765650150a7d54909345c201f2
Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
2022-01-23 22:26:27 +0300

    Use time dialog in deal form.

--- app/src/main/kotlin/org/medicine/ui/screen/dealform/DealFormView.kt
+++ app/src/main/kotlin/org/medicine/ui/screen/dealform/DealFormView.kt
@@ -1,13 +1,17 @@
 package org.medicine.ui.screen.dealform
 
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import kotlinx.coroutines.CoroutineScope
 import org.medicine.ui.common.composable.successfulinfo.ObjectDeleteSuccessful
 import org.medicine.ui.common.composable.successfulinfo.ObjectSaveSuccessful
+import org.medicine.ui.common.dialog.showDatePickerDialog
+import org.medicine.ui.common.dialog.showTimePickerDialog
 import org.medicine.ui.common.model.MedicalFormViewState
 import org.medicine.ui.screen.dealform.composable.DealFrom
 import org.medicine.ui.screen.dealform.model.DealFormModel
@@ -26,6 +30,7 @@ internal fun DealFormView(
   saveOnSuccessful: suspend CoroutineScope.(Long) -> Unit,
   deleteOnSuccessful: suspend CoroutineScope.() -> Unit,
 ) {
+  val activity = LocalContext.current as AppCompatActivity
 
   Box(
     modifier = Modifier.fillMaxSize(),
@@ -39,8 +44,8 @@ internal fun DealFormView(
           `object`,
           { dealFormOnChange(`object`.copy(name = it)) },
           { dealFormOnChange(`object`.copy(description = it)) },
-          {  },
-          {  },
+          { showDatePickerDialog(activity, `object`.date) { dealFormOnChange(`object`.copy(date = it)) } },
+          { showTimePickerDialog(activity, `object`.time) { dealFormOnChange(`object`.copy(time = it)) } },
           { createOrSaveDeal(-1L, objectId, `object`) },
           { objectId?.let(deleteDeal) },
         )
