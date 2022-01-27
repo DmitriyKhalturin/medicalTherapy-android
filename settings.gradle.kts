582bfc5b7c56992e61a7ec80c09fdc231c8b73b7
Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
2022-01-28 00:39:50 +0300

    Move tools to git submodule.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,3 +6,4 @@ include(":tools")
 
 project(":schedule").projectDir = File(rootDir, "MedicalTherapy-Schedule")
 project(":source").projectDir = File(rootDir, "MedicalTherapy-Source")
+project(":tools").projectDir = File(rootDir, "WearTools")
